12627b9c036547bd516cb4b4434894678722027e
Zane Schaffer <personal.zane@gmail.com>
2023-02-19 12:55:08 -0800

    add recipe

---
+++ components/recipe.tsx
@@ -0,0 +1,13 @@
+export default function Recipe() {
+  return (
+    <>
+      <select className="select w-full max-w-xs">
+        <option disabled selected>
+          Brewing Recipe
+        </option>
+        <option>James Hoffman V60</option>
+        <option>Kalita Wave</option>
+      </select>
+    </>
+  );
+}
