cedf6bda8e6dd26d22621dbdfd82678e0a1004a6
Zane Schaffer <personal.zane@gmail.com>
2023-02-19 12:56:13 -0800

    replace index with hero

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,9 @@
-import Timer from "@/components/timer";
-import History from "@/components/history";
-import Head from "next/head";
+import Hero from "@/components/hero";
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>Bloom</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className={""}>
-        <Timer />
-        <History />
-      </main>
-    </>
+    <main className={"flex flex-col w-full"}>
+      <Hero />
+    </main>
   );
 }
