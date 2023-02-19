1eaba2e7ded9d07e794b111cb49480037d126b2b
Zane Schaffer <personal.zane@gmail.com>
2023-02-19 12:53:27 -0800

    add Navbar

---
+++ components/navbar.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <div className="navbar bg-base-100">
+      <div className="flex-1">
+        <Link href="/" className="btn btn-ghost normal-case text-xl">
+          bloom
+        </Link>
+      </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <Link href="/history">History</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
