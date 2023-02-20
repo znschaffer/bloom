25dea5d25027cefa8136f72fdec6fa07f5282436
Zane Schaffer <personal.zane@gmail.com>
2023-02-20 14:07:48 -0800

    reorganize index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,17 @@
+import Brew from "@/components/brew";
 import Hero from "@/components/hero";
+import History from "@/components/history";
 
 export default function Home() {
   return (
-    <main className={"flex flex-col w-full"}>
-      <Hero />
-    </main>
+    <>
+      <main className={"flex flex-col"}>
+        <Hero />
+        <div className={"hero"}>
+          <Brew />
+        </div>
+        <History />
+      </main>
+    </>
   );
 }
