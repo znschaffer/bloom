06836defb7c8b6880afa1f2c95d9b0ba06ac86c4
Zane Schaffer <personal.zane@gmail.com>
2023-02-20 14:06:25 -0800

    update styling

--- components/brew.tsx
+++ components/brew.tsx
@@ -1,18 +1,20 @@
-import { createContext } from "vm";
+import { useState } from "react";
 import Recipe from "./recipe";
 import Steps from "./steps";
 import Timer from "./timer";
 
 export default function Brew() {
-  const BrewContext = createContext();
+  const [recipe, setRecipe] = useState(null);
   return (
-    <div className="flex">
-      <div className="m-3">
-        <Timer />
-      </div>
-      <div className="m-3">
-        <Recipe />
-        <Steps />
+    <div className="card m-8 border bg-base-100 border-base-200 max-w-[80rem]">
+      <div className="card-body ">
+        <div className="flex flex-col lg:flex-row items-center gap-8">
+          <div className="flex flex-col gap-8">
+            <Recipe setRecipe={setRecipe} />
+            <Timer />
+          </div>
+          <Steps recipe={recipe} />
+        </div>
       </div>
     </div>
   );
