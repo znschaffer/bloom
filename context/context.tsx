1b4ec59487c5356138cb27bd98ff0fc3f2586c98
Zane Schaffer <personal.zane@gmail.com>
2023-02-20 14:07:17 -0800

    add types to useContext

--- context/context.tsx
+++ context/context.tsx
@@ -1,13 +1,37 @@
-import { createContext, useState } from "react";
+import {
+  createContext,
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  useState,
+} from "react";
 
-export const Timer_data = createContext(null);
+export type TimerContent = {
+  timer: {
+    brewing: boolean;
+    time: number;
+  };
+  setTimer: Dispatch<
+    SetStateAction<{
+      brewing: boolean;
+      time: number;
+    }>
+  >;
+} | null;
 
-export default function Context({ children }) {
+export const Timer_data = createContext<TimerContent>(null);
+
+type ContextProps = {
+  children: ReactNode;
+};
+
+export default function Context({ children }: ContextProps) {
   const [timer, setTimer] = useState({ brewing: false, time: 0 });
-
   return (
-    <Timer_data.Provider value={{ timer, setTimer }}>
-      {children}
-    </Timer_data.Provider>
+    <>
+      <Timer_data.Provider value={{ timer, setTimer }}>
+        {children}
+      </Timer_data.Provider>
+    </>
   );
 }
