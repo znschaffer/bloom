c7768a26341442b7cff967a6b2c0d66a896f5a3d
Zane Schaffer <personal.zane@gmail.com>
2023-02-20 13:33:56 -0800

    add recipe picker component

--- components/recipe.tsx
+++ components/recipe.tsx
@@ -1,12 +1,72 @@
-export default function Recipe() {
+import { useEffect, useState } from "react";
+import Timer from "./timer";
+
+export const baseRecipes = {
+  "James Hoffman V60": {
+    name: "James Hoffman V60",
+    author: "James Hoffman",
+    method: "Pour Over",
+    ratio: {
+      water: 500,
+      beans: 30,
+    },
+    link: null,
+    steps: [
+      { time: 0, action: "Pour 60g" },
+      { time: 45, action: "Pour 240g" },
+      { time: 75, action: "Pour 200g" },
+      { time: 120, action: "Swirl V60 gently" },
+      { time: 180, action: "Done!" },
+    ],
+  },
+  "Scott Rao V60": {
+    name: "Scott Rao V60",
+    author: "Scott Rao",
+    method: "Pour Over",
+    ratio: {
+      water: 360,
+      beans: 22,
+    },
+    link: null,
+    steps: [
+      { time: 0, action: "Pour 66g" },
+      { time: 45, action: "Pour 300g" },
+      { time: 105, action: "Swirl V60 gently" },
+      { time: 180, action: "Done!" },
+    ],
+  },
+};
+
+export default function Recipe({ setRecipe }) {
+  const initialState = {};
+  const [recipes, setRecipes] = useState(initialState);
+  useEffect(() => {
+    const recipeData = JSON.parse(localStorage.getItem("recipes"));
+    if (recipeData) {
+      setRecipes(recipeData);
+    }
+    if (Object.keys(recipeData).length == 0) {
+      setRecipes(baseRecipes);
+    }
+  }, []);
+
+  function selectRecipe(e) {
+    setRecipe(recipes[e.target.value]);
+  }
+
   return (
     <>
-      <select className="select w-full max-w-xs">
-        <option disabled selected>
-          Brewing Recipe
+      <select
+        onChange={selectRecipe}
+        defaultValue="Brew Recipe"
+        className=" bg-base-100 select select-sm select-ghost "
+      >
+        <option key="brew recipe" disabled>
+          Brew Recipe
         </option>
-        <option>James Hoffman V60</option>
-        <option>Kalita Wave</option>
+        {Object.keys(recipes).map((el, i) => (
+          <option key={i}>{el}</option>
+        ))}
       </select>
     </>
   );
