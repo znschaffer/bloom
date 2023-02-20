f993d775be54d1af7157a325c33e1052c55760e8
Zane Schaffer <personal.zane@gmail.com>
2023-02-20 13:33:33 -0800

    remove history add recipes to navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -11,7 +11,7 @@ export default function Navbar() {
       <div className="flex-none">
         <ul className="menu menu-horizontal px-1">
           <li>
-            <Link href="/history">History</Link>
+            <Link href="/recipes">Recipes</Link>
           </li>
         </ul>
       </div>
